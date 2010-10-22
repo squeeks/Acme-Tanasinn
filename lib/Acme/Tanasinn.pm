e79eb6fed67878873ad606d416fc340c00b97858
Squeeks <squeek@cpan.org>
2010-10-22 02:45:55 +0100

    Matching POD version with actual version.

--- lib/Acme/Tanasinn.pm
+++ lib/Acme/Tanasinn.pm
@@ -14,11 +14,11 @@ Acme::Tanasinn - Don't think. Feel and you'll be tanasinn.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
